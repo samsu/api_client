2f85ab9557975054ad68b32efcc5c7654a0b99d7
samsu <susltd.su@gmail.com>
2017-05-03 14:54:55 -0700

    add test

---
+++ test/apiclient.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import time
+from fortiosclient import FortiosApiClient as FortiosApiClient
+
+if __name__ == "__main__":
+    api = [("10.160.37.98", 443, True)]
+    user = "admin"
+    password = ""
+    cli = FortiosApiClient(api, user, password)
+    print "----------"
+    message = {
+        "name": "ext_4093",
+        "vlanid": 4093,
+        "vdom": "root",
+        "interface": "port1",
+        "ip": "192.168.30.254 255.255.255.0"
+    }
+    cli.request("ADD_VLAN_INTERFACE", message)
+    message = {
+        "name": "port5",
+        "vdom": "root",
+        "ip": "192.168.40.254 255.255.255.0"
+        #"secondaryips": ["192.168.20.200 255.255.255.0", ]
+        #"secondaryips": []
+    }
+    print cli.request("SET_VLAN_INTERFACE", message)
+    message = {
+        "name": "ext_4093"
+    }
+
+    #print cli.request("GET_VLAN_INTERFACE", message)
+
