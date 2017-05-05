8b5f2bdda542940de47e94e401bccfb16d849a5a
samsu <susltd.su@gmail.com>
2017-05-04 17:04:44 -0700

    first msg

--- test/apiclient.py
+++ test/apiclient.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 import time
+import os
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+os.sys.path.insert(0,parentdir)
+
 from client import FortiosApiClient as FortiosApiClient
 
 if __name__ == "__main__":
     api = [("10.160.37.98", 443, True)]
     user = "admin"
-    password = ""
+    password = "lRFUJKlKYTOM17fK6UYP8lxVS84bEj3N1gkUvUZ"
     cli = FortiosApiClient(api, user, password)
     print "----------"
     message = {
@@ -16,7 +23,7 @@ if __name__ == "__main__":
         "interface": "port1",
         "ip": "192.168.30.254 255.255.255.0"
     }
-    cli.request("ADD_VLAN_INTERFACE", message)
+    #cli.request("ADD_VLAN_INTERFACE", message)
     message = {
         "name": "port5",
         "vdom": "root",
@@ -24,10 +31,9 @@ if __name__ == "__main__":
         #"secondaryips": ["192.168.20.200 255.255.255.0", ]
         #"secondaryips": []
     }
-    print cli.request("SET_VLAN_INTERFACE", message)
+    #print cli.request("SET_VLAN_INTERFACE", message)
     message = {
         "name": "ext_4093"
     }
 
     #print cli.request("GET_VLAN_INTERFACE", message)
-
