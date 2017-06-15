9d0be03d3aa074e45ecc9775e529fcb054c04361
samsu <susltd.su@gmail.com>
2017-06-15 10:46:21 -0700

    first msg

--- test/apiclient.py
+++ test/apiclient.py
@@ -7,13 +7,13 @@ import inspect
 import api_client.client as client
 
 
-FortiosApiClient=client.FortiosApiClient
+ApiClient=client.FortiAuthApiClient
 
 if __name__ == "__main__":
     api = [("10.160.37.98", 443, True)]
     user = "admin"
     password = "lRFUJKlKYTOM17fK6UYP8lxVS84bEj3N1gkUvUZ"
-    cli = FortiosApiClient(api, user, password)
+    cli = ApiClient(api, user, password)
     cli.request('GET_USERGROUPS')
     print "----------"
     message = {
