53856f21426be965661ed3c098658be8339f3d48
samsu <susltd.su@gmail.com>
2017-06-26 15:58:16 -0700

    first msg

--- test/apiclient.py
+++ test/apiclient.py
@@ -14,14 +14,14 @@ if __name__ == "__main__":
     user = "admin"
     password = "slRFUJKlKYTOM17fK6UYP8lxVS84bEj3N1gkUvUZ"
     cli = ApiClient(api, user, password)
-    print "----TESTING USER_GROUPS------"
+    #print "----TESTING USER_GROUPS------"
     message = {
         "name": "test123"
     }
-    print cli.request('GET_USERGROUPS')
-    print cli.request("CREATE_USERGROUPS", **message)
-    print cli.request('GET_USERGROUPS', **message)
-    print cli.request("DELETE_USERGROUPS", **message)
+    #print cli.request('GET_USERGROUPS')
+    #print cli.request("CREATE_USERGROUPS", **message)
+    #print cli.request('GET_USERGROUPS', **message)
+    #print cli.request("DELETE_USERGROUPS", **message)
 
     print "----TESTING USERS------"
     message = {
