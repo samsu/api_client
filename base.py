fda43b614ca6ef8f45976bd7bcc747464815b2c3
samsu <susltd.su@gmail.com>
2017-07-06 16:22:26 -0700

    init commit

--- base.py
+++ base.py
@@ -327,6 +327,9 @@ class HTTPSClientAuthConnection(httplib.HTTPSConnection):
 
     def __init__(self, host, port, key_file, cert_file, ca_file,
                  ssl_sni=None, timeout=None):
+        #httplib.HTTPSConnection.__init__(self, host, port,
+        #                                 key_file=key_file,
+        #                                 cert_file=cert_file)
         super(HTTPSClientAuthConnection, self).__init__(host, port,
                                                         key_file=key_file,
                                                         cert_file=cert_file)
