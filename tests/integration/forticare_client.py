1c2cf9ebc67ae9b27d242377fe9201ee48b73d16
samsu <susltd.su@gmail.com>
2017-07-07 16:51:12 -0700

    init commit

--- tests/integration/forticare_client.py
+++ tests/integration/forticare_client.py
@@ -11,15 +11,24 @@ ApiClient=client.FortiCareApiClient
 
 if __name__ == "__main__":
     api = [("172.30.38.89", 443, True)]
+
+    # Sub CA
+    print "----TESTING with Sub CA certificate ------"
     key_file = "/root/subca/cert201706291056.key"
     cert_file = "/root/subca/cert201706291056.crt"
     cert_reqs = "/root/subca/cert201706291056.csr"
     ca_file = "/root/subca/chain.pem"
     server_hostname = "fortinet-ca2.fortinet.com"
-
     cli = ApiClient(api, key_file=key_file,
                     cert_file=cert_file, ca_file=ca_file,
                     ssl_sni=server_hostname)
+    cli.request('TEST')
 
-    print "----TESTING ------"
+    # CA
+    print "----TESTING with CA certificate ------"
+    key_file = "/root/ca/cert201706291056.key"
+    cert_file = "/root/ca/cert201706291056.crt"
+    ca_file = "/root/ca/cacertkey1-sha2.pem"
+    cli = ApiClient(api, key_file=key_file,
+                    cert_file=cert_file, ca_file=ca_file)
     cli.request('TEST')
