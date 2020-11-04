ba21e2a40d7a24b5f36d8c11e69de1020ad59e50
Jingwen Li <jwli@fortinet.com>
2020-11-04 15:41:25 -0800

    Fix template of fas

--- api_client/templates/fas.py
+++ api_client/templates/fas.py
@@ -364,6 +364,7 @@ GET_AUTH = """
     {% else %}
         {% set _options = {
             "sn": sn,
+            "chunksize": chunksize
         } %}
         {% set _query = [] %}
         {% for k, v in _options.items() if v is defined %}
@@ -444,9 +445,6 @@ ADD_TRIAL = """
     "body": {
         {% if sn is defined %}
             "sn": "{{ sn }}"
-        {% endif %}
-        {% if customer_id is defined %}
-            "customer_id": "{{ customer_id }}"
         {% endif %}
     }
 }
