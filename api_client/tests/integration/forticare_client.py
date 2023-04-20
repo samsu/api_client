28d809d957eee4ae978e950f3c364b89eb8b8f65
samsu <susltd.su@gmail.com>
2023-04-20 13:03:13 -0700

    update related outdated testcases

--- api_client/tests/integration/forticare_client.py
+++ api_client/tests/integration/forticare_client.py
@@ -6,25 +6,27 @@ ApiClient = client.FortiCareApiClient
 
 
 if __name__ == "__main__":
-    api = [("172.30.38.89", 443, True)]
-
+    api = [("127.0.0.1", 443, True)]
+    # replace the email and
+    email = "***@***.com"
     # Sub CA
-    print "----TESTING API client with Sub CA certificate ------"
-    key_file = "/root/subca/cert201706291056.key"
-    cert_file = "/root/subca/cert201706291056.crt"
-    #cert_reqs = "/root/subca/cert201706291056.csr"
-    ca_file = "/root/subca/chain.pem"
+    print("----TESTING API client with Sub CA certificate ------")
+    # replace related cert information with your own in the test
+    key_file = "./xxx.key"
+    cert_file = "./xxx.crt"
+    ca_file = "./xxx.pem"
     server_hostname = "fortinet-ca2.fortinet.com"
     cli = ApiClient(api, key_file=key_file,
                     cert_file=cert_file, ca_file=ca_file,
                     ssl_sni=server_hostname)
-    cli.request('TEST')
+    cli.request('GET_ACCOUNT', email=email)
 
     # CA
-    print "----TESTING API client with CA certificate ------"
-    key_file = "/root/ca/cert201706291056.key"
-    cert_file = "/root/ca/cert201706291056.crt"
-    ca_file = "/root/ca/cacertkey1-sha2.pem"
+    print("----TESTING API client with CA certificate ------")
+    # replace related cert information with your own in the test
+    key_file = "./xxx.key"
+    cert_file = "./xxx.crt"
+    ca_file = "./xxx.pem"
     cli = ApiClient(api, key_file=key_file,
                     cert_file=cert_file, ca_file=ca_file)
-    cli.request('TEST')
+    cli.request('GET_ACCOUNT', email=email)
