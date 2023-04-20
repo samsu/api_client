0ffa33ac88aa70df13a7cc1744cd0333ef80fc55
samsu <susltd.su@gmail.com>
2023-04-20 13:03:13 -0700

    update related outdated testcases

--- api_client/tests/integration/fas_client.py
+++ api_client/tests/integration/fas_client.py
@@ -6,27 +6,15 @@ import api_client.fas_client as client
 ApiClient = client.FASApiClient
 
 if __name__ == "__main__":
-    api = [("10.160.37.61", 443, True)]
+    api = [("127.0.0.1", 443, True)]
     # CA
-    print("----TESTING API client with CA certificate ------")
-    # sn is 'FG60DP4615001748' in the test
-    key_file = "/root/fgt/box/fgt_b.key"
-    cert_file = "/root/fgt/box/fgt_b.crt"
-    ca_file = "/root/fgt/box/test_root_Fortinet_CA.cer"
+    # replace the sn and cert with your own in the test
+    sn = 'Fxxxxxxxxx'
+    key_file = "./xxx.key"
+    cert_file = "./xxx.crt"
+    ca_file = "./xxx.cer"
 
     cli = ApiClient(api, key_file=key_file,
                     cert_file=cert_file, ca_file=ca_file)
-    res = cli.request('GET_ACTIVATION', sn='FG60DP4615001748', vdom='root')
-    print("GET_ACTIVATION = ", res)
-    if not res:
-        res = cli.request('ADD_ACTIVATION', sn='FG60DP4615001748',
-                          vdom='root', namespace='default')
-        print("ADD_ACTIVATION = ", res)
-    if isinstance(res, list):
-        res_delete = []
-        for record in res:
-            _res = cli.request('DELETE_ACTIVATION', id=record['id'])
-            res_delete.append(_res)
-    elif isinstance(res, dict):
-        res_delete = cli.request('DELETE_ACTIVATION', id=res['id'])
-    print("DELETE_ACTIVATION = ", res_delete)
+    print("----TESTING API client with CA certificate ------")
+    res = cli.request('GET_USER', sn=sn, vdom='root')
--- api_client/tests/integration/fortiauth_client.py
+++ api_client/tests/integration/fortiauth_client.py
@@ -10,49 +10,17 @@ import api_client.fortiauth_client as client
 ApiClient=client.FortiAuthApiClient
 
 if __name__ == "__main__":
-    api = [("10.160.37.98", 443, True)]
-    user = "admin"
-    password = "slRFUJKlKYTOM17fK6UYP8lxVS84bEj3N1gkUvUZ"
+    api = [("127.0.0.1", 443, True)]
+    # replace the user and password with your own
+    user = "test"
+    password = "************************"
     cli = ApiClient(api, user, password)
-    # print("----TESTING USER_GROUPS------")
+
+    print("----TESTING USERGROUPS------")
     message = {
         "name": "test123"
     }
-    print("----TESTING USERGROUPS------")
     cli.request('GET_USERGROUPS')
     cli.request("CREATE_USERGROUP", **message)
     cli.request('GET_USERGROUPS', **message)
     cli.request("DELETE_USERGROUP", **message)
-
-    print("----TESTING USERS------")
-    message = {
-        "username": "test_1"
-    }
-
-    cli.request("GET_USERS", **message)
-    add_message = {
-        "username": "test_1",
-        "token_auth": "true",
-        "ftk_only": "false",
-        "ftm_act_method": "email",
-        "token_type": "sms",
-        "email": "sansu@fortinet.com",
-        "mobile_number": "+1-4085026006",
-        "active": "true"
-    }
-    cli.request("CREATE_USER", **add_message)
-    res = cli.request("GET_USERS", **message)
-    id = str(res[0]['id'])
-    cli.request("MODIFY_USER", id=id, first='changed')
-    cli.request("GET_USERS",  **message)
-    cli.request("DELETE_USER", id=id)
-
-    message = {
-        "status": "available"
-    }
-    print("----TESTING FORTITOKENS------")
-    cli.request("GET_FORTITOKENS", **message)
-    cli.request("GET_FORTITOKENS")
-
-    print("----TESTING SYSTEMINFO------")
-    cli.request("GET_SYSTEMINFO")
